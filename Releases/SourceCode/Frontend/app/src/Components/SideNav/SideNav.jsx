456a25dcfbe8a8b2c2a8f20723205b4bf824e2b8
wishva perera <vibushitha99@gamil.com>
2021-08-16 00:56:00 +0530

    initial commit

---
+++ Releases/SourceCode/Frontend/app/src/Components/SideNav/SideNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./SideNav"
+import { imagePath } from '../../Services'
+
+export default function SideNav() {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  )
+}
