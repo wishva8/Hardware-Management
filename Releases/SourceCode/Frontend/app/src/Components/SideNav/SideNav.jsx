1fff823b9f6f880f0c086e8277105a9d3a20726d
wishva perera <vibushitha99@gamil.com>
2021-08-18 00:23:12 +0530

    sidebar updated

--- Releases/SourceCode/Frontend/app/src/Components/SideNav/SideNav.jsx
+++ Releases/SourceCode/Frontend/app/src/Components/SideNav/SideNav.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./SideNav.css";
 import { imagePath } from "../../Services";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTachometerAlt } from "@fortawesome/free-solid-svg-icons";
+import { faFileAlt } from "@fortawesome/free-solid-svg-icons";
+import { faTruck} from "@fortawesome/free-solid-svg-icons";
+import { faDatabase} from "@fortawesome/free-solid-svg-icons";
+import { faIdCard} from "@fortawesome/free-solid-svg-icons";
 
 export default function SideNav() {
   return (
@@ -17,39 +23,59 @@ export default function SideNav() {
         </a>
         <hr />
         <ul className="nav nav-pills flex-column mb-auto">
+          <li>
+            <a href="#" className="nav-link mt-2 mb-2 active align-middle">
+              <div className="row">
+                <div className="col-4">
+                  <FontAwesomeIcon icon={faTachometerAlt} size="2x" />
+                </div>
+                <div className="col-8 text-start mt-2 ">Dashboard</div>
+              </div>
+            </a>
+          </li>
           <li className="nav-item">
-            <a href="#" className="nav-link text-white" aria-current="page">
-              <svg className="bi me-2" width="16" height="16"></svg>
-              Home
+            <a href="#" className="nav-link mt-2 mb-2 text-white" aria-current="page">
+              <div className="row">
+                <div className="col-4">
+                  <FontAwesomeIcon icon={faFileAlt} size="2x" />
+                </div>
+                <div className="col-8 text-start mt-2">Orders</div>
+              </div>
             </a>
           </li>
           <li>
-            <a href="#" className="nav-link active">
-              <svg className="bi me-2" width="16" height="16"></svg>
-              Dashboard
+            <a href="#" className="nav-link mt-2 mb-2 text-white">
+              <div className="row">
+                <div className="col-4">
+                  <FontAwesomeIcon icon={faDatabase} size="2x" />
+                </div>
+                <div className="col-8 text-start mt-2">Inventory</div>
+              </div>
             </a>
           </li>
           <li>
-            <a href="#" className="nav-link text-white">
-              <svg className="bi me-2" width="16" height="16"></svg>
-              Orders
+            <a href="#" className="nav-link mt-2 mb-2 text-white">
+              <div className="row">
+                <div className="col-4">
+                  <FontAwesomeIcon icon={faTruck} size="2x" />
+                </div>
+                <div className="col-8 text-start mt-2">Deliveries</div>
+              </div>
             </a>
           </li>
           <li>
-            <a href="#" className="nav-link text-white">
-              <svg className="bi me-2" width="16" height="16"></svg>
-              Products
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-              <svg className="bi me-2" width="16" height="16"></svg>
-              Customers
+            <a href="#" className="nav-link mt-2 mb-2 text-white">
+              <div className="row">
+                <div className="col-4">
+                  <FontAwesomeIcon icon={faIdCard} size="2x" />
+                </div>
+                <div className="col-8 text-start mt-2">Drivers</div>
+              </div>
             </a>
           </li>
         </ul>
         <hr />
-        <div className="dropdown">
+        {/* <div className="dropdown">
           <a
             href="#"
             className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
@@ -94,7 +120,7 @@ export default function SideNav() {
               </a>
             </li>
           </ul>
-        </div>
+        </div> */}
       </div>
     </div>
   );
