9f3a2d30cc1a61caf49faae6f278b0a58fe8a7c8
wishva perera <vibushitha99@gamil.com>
2021-08-18 01:04:14 +0530

    admin dashboard initialized

--- Releases/SourceCode/Frontend/app/src/App.js
+++ Releases/SourceCode/Frontend/app/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import './App.css';
 import SideNav from './Components/SideNav/SideNav';
+import AdminDashboard from "./Pages/AdminDashboard/AdminDashboard"
 function App() {
   return (
     <div className="App">
-      <SideNav/>
+      <BrowserRouter>
+        <Switch>
+          <SideNav />
+          <Route path="">
+            <AdminDashboard />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
