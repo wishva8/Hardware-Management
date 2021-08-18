ae4b24596d22e1b749a85bcc417bdf64d3694bd1
tlorence <tirone.lorence@gmail.com>
2021-08-18 22:52:52 +0530

    Order list added

---
+++ Releases/SourceCode/Frontend/app/src/Pages/OrderList/OrderList.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class OrderList extends Component {
+    render() {
+        return (
+            <div className="text-center">
+                <h1 className="text-white text-center" >Hello World</h1>
+                
+            </div>
+        )
+    }
+}
