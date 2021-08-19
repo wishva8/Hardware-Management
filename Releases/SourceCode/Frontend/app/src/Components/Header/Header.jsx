aebc6fc9febb28d8808cf81ff5a0488c11934b32
wishva perera <vibushitha99@gamil.com>
2021-08-19 23:16:42 +0530

    header padding changed

--- Releases/SourceCode/Frontend/app/src/Components/Header/Header.jsx
+++ Releases/SourceCode/Frontend/app/src/Components/Header/Header.jsx
@@ -3,56 +3,58 @@ import "./Header.css";
 
 export default function Header(props) {
   return (
-    <div className="header row">
-      <div className="col">
-        <h5 className="ms-4 mt-1 text-green">{props.topic ?? ''}</h5>
-      </div>
-      <div className="col d-flex justify-content-end">
-        <div className="dropdown">
-          <a
-            href="#"
-            className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-            id="dropdownUser1"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <img
-              src="https://github.com/mdo.png"
-              alt=""
-              width="32"
-              height="32"
-              className="rounded-circle me-2"
-            />
-            <strong>mdo</strong>
-          </a>
-          <ul
-            className="dropdown-menu dropdown-menu-dark text-small shadow"
-            aria-labelledby="dropdownUser1"
-          >
-            {/* <li>
+    <div className="header">
+      <div className="row">
+        <div className="col">
+          <h5 className="ms-4 mt-1 text-green">{props.topic ?? ""}</h5>
+        </div>
+        <div className="col d-flex justify-content-end">
+          <div className="dropdown">
+            <a
+              href="#"
+              className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+              id="dropdownUser1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <img
+                src="https://github.com/mdo.png"
+                alt=""
+                width="32"
+                height="32"
+                className="rounded-circle me-2"
+              />
+              <strong>mdo</strong>
+            </a>
+            <ul
+              className="dropdown-menu dropdown-menu-dark text-small shadow"
+              aria-labelledby="dropdownUser1"
+            >
+              {/* <li>
               <a className="dropdown-item" href="#">
                 New project...
               </a>
             </li> */}
-            <li>
-              <a className="dropdown-item" href="#">
-                Settings
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Profile
-              </a>
-            </li>
-            <li>
-              <hr className="dropdown-divider" />
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Sign out
-              </a>
-            </li>
-          </ul>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Settings
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Profile
+                </a>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Sign out
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
