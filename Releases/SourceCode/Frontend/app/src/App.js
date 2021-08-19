6ad1d4582624fe800dbfde0d3f8763a7e83302d1
wishva perera <vibushitha99@gamil.com>
2021-08-19 23:58:08 +0530

    sidenav remove from app.js

--- Releases/SourceCode/Frontend/app/src/App.js
+++ Releases/SourceCode/Frontend/app/src/App.js
@@ -1,6 +1,5 @@
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import './App.css';
-import SideNav from './Components/SideNav/SideNav';
 import AdminDashboard from "./Pages/AdminDashboard/AdminDashboard"
 import OrderList from "./Pages/OrderList/OrderList";
 function App() {
@@ -8,7 +7,6 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Switch>
-          {/* <SideNav /> */}
           <Route path="/orderList">
             <OrderList/>
           </Route>
