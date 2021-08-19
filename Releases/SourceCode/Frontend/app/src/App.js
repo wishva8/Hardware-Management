ba99167a1ddf947c49e1e69a727529b00216e3d1
wishva perera <vibushitha99@gamil.com>
2021-08-20 00:04:31 +0530

    login hure

--- Releases/SourceCode/Frontend/app/src/App.js
+++ Releases/SourceCode/Frontend/app/src/App.js
@@ -10,7 +10,7 @@ function App() {
           <Route path="/orderList">
             <OrderList/>
           </Route>
-          <Route path="">
+          <Route path="/dashboard">
             <AdminDashboard />
           </Route>
         </Switch>
