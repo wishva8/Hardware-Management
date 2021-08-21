d18202cc4d951a0e4cacb2dacc144717add58123
Himantha_Amarathunga <Himanthaa@gmail.com>
2021-08-21 13:10:00 +0530

    User Login App.js implementation

--- Releases/SourceCode/Frontend/app/src/App.js
+++ Releases/SourceCode/Frontend/app/src/App.js
@@ -35,6 +35,9 @@ function App() {
           <Route path="/createItem">
             <CreateItem />
           </Route>
+          <Route path="/loginUser">
+            <UserLogin />
+          </Route>
         </Switch>
       </BrowserRouter>
     </div>
