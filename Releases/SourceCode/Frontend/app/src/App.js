359db90868f6630ad181b7fef3d68e6fbe16ec15
Savvy9899 <pamuditha.rasanjana99@gmail.com>
2021-08-22 22:50:23 +0530

    Delivery Update done

--- Releases/SourceCode/Frontend/app/src/App.js
+++ Releases/SourceCode/Frontend/app/src/App.js
@@ -14,6 +14,7 @@ import CreateOrder from "./Pages/OrderList/CreateOrder";
 import UpdateOrder from "./Pages/OrderList/UpdateOrder";
 import CreateDelivery from "./Pages/Delivery/CreateDelivery";
 import DeliveryList from "./Pages/Delivery/DeliveryList";
+import UpdateDelivery from "./Pages/Delivery/UpdateDelivery";
 
 function App() {
   return (
@@ -61,6 +62,9 @@ function App() {
           </Route>
           <Route path="/deliveryList">
             <DeliveryList />
+          </Route>
+          <Route path="/updateDelivery">
+            <UpdateDelivery />
           </Route>
         </Switch>
       </BrowserRouter>
