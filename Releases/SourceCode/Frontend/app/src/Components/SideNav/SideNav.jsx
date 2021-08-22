38def8f4e1cf1f26e4bbf0735d80f1e96a4f8dda
Wishva Perera <vibushitha99@gmail.com>
2021-08-22 22:45:55 +0530

    resized admin dashboard

--- Releases/SourceCode/Frontend/app/src/Components/SideNav/SideNav.jsx
+++ Releases/SourceCode/Frontend/app/src/Components/SideNav/SideNav.jsx
@@ -4,14 +4,16 @@ import { imagePath } from "../../Services";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTachometerAlt } from "@fortawesome/free-solid-svg-icons";
 import { faFileAlt } from "@fortawesome/free-solid-svg-icons";
-import { faTruck} from "@fortawesome/free-solid-svg-icons";
-import { faDatabase} from "@fortawesome/free-solid-svg-icons";
-import { faIdCard} from "@fortawesome/free-solid-svg-icons";
+import { faTruck } from "@fortawesome/free-solid-svg-icons";
+import { faDatabase } from "@fortawesome/free-solid-svg-icons";
+import { faIdCard } from "@fortawesome/free-solid-svg-icons";
+import { Link, NavLink } from "react-router-dom";
 
 export default function SideNav() {
   return (
     <div>
       <div className="d-flex flex-column flex-shrink-0 p-3 sidebar">
+        {/* <Link to="/dashboard"> */}
         <a
           href="/"
           className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
@@ -21,61 +23,81 @@ export default function SideNav() {
           </div>
           <span className="fs-4"></span>
         </a>
+        {/* </Link> */}
         <hr />
         <ul className="nav nav-pills flex-column mb-auto">
           <li>
-            <a href="#" className="nav-link mt-2 mb-2 active align-middle">
+            <NavLink
+              to="/dashboard"
+              activeClassName="active"
+              className="nav-link mt-2 mb-2 text-white"
+            >
               <div className="row">
                 <div className="col-4">
                   <FontAwesomeIcon icon={faTachometerAlt} size="2x" />
                 </div>
                 <div className="col-8 text-start mt-2 ">Dashboard</div>
               </div>
-            </a>
+            </NavLink>
           </li>
           <li className="nav-item">
-            <a href="#" className="nav-link mt-2 mb-2 text-white" aria-current="page">
+            <NavLink
+              to="/orderList"
+              activeClassName="active"
+              className="nav-link mt-2 mb-2 text-white"
+            >
               <div className="row">
                 <div className="col-4">
                   <FontAwesomeIcon icon={faFileAlt} size="2x" />
                 </div>
                 <div className="col-8 text-start mt-2">Orders</div>
               </div>
-            </a>
+            </NavLink>
           </li>
           <li>
-            <a href="#" className="nav-link mt-2 mb-2 text-white">
+            <NavLink
+              to="/inventoryList"
+              activeClassName="active"
+              className="nav-link mt-2 mb-2 text-white"
+            >
               <div className="row">
                 <div className="col-4">
                   <FontAwesomeIcon icon={faDatabase} size="2x" />
                 </div>
                 <div className="col-8 text-start mt-2">Inventory</div>
               </div>
-            </a>
+            </NavLink>
           </li>
           <li>
-            <a href="#" className="nav-link mt-2 mb-2 text-white">
+            <NavLink
+              to="/delivery"
+              activeClassName="active"
+              className="nav-link mt-2 mb-2 text-white"
+            >
               <div className="row">
                 <div className="col-4">
                   <FontAwesomeIcon icon={faTruck} size="2x" />
                 </div>
                 <div className="col-8 text-start mt-2">Deliveries</div>
               </div>
-            </a>
+            </NavLink>
           </li>
           <li>
-            <a href="#" className="nav-link mt-2 mb-2 text-white">
+            <NavLink
+              to="/driverList"
+              activeClassName="active"
+              className="nav-link mt-2 mb-2 text-white"
+            >
               <div className="row">
                 <div className="col-4">
                   <FontAwesomeIcon icon={faIdCard} size="2x" />
                 </div>
                 <div className="col-8 text-start mt-2">Drivers</div>
               </div>
-            </a>
+            </NavLink>
           </li>
         </ul>
         <hr />
-        
       </div>
     </div>
   );
