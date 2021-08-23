33ee8e910034dbac5ce15883e458b8d9befe860f
tlorence <tirone.lorence@gmail.com>
2021-08-23 22:22:08 +0530

    Order

--- Releases/SourceCode/Frontend/app/src/Pages/OrderList/OrderList.jsx
+++ Releases/SourceCode/Frontend/app/src/Pages/OrderList/OrderList.jsx
@@ -5,9 +5,39 @@ import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 import Header from "../../Components/Header/Header";
 import SideNav from "../../Components/SideNav/SideNav";
 import { faDownload, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { getOrders } from "../../Services/orders";
+import axios from "axios";
+import { orderURL } from "../../Services/endpoints";
 
 export default class OrderList extends Component {
+  state = {
+    orderId: "",
+    description: "",
+    itemId: "",
+    quantity: "",
+    unitPrice: "",
+    totalPrice: "",
+    date: "",
+    customerName: "",
+    customerPhoneNo: "",
+    status: "",
+    orders: [],
+  };
+
+  async componentDidMount() {
+    const orders = await axios.get(orderURL).then((result) => {
+      // console.log(result.data);
+      this.setState({
+        orders: result.data,
+      });
+    });
+
+    // console.log(this.state.orders);
+  }
+
   render() {
+    //  const data=this.state.orders;
+    const { orders } = this.state;
     return (
       <div>
         <SideNav />
@@ -22,64 +52,33 @@ export default class OrderList extends Component {
             </button>
           </div>
           <div className="row">
-            <table className="table table-bordered  order">
-              <tr className="orderListItems">
-                <th className="ps-4">Order Id</th>
-                <th className="ps-4">Description</th>
-                <th className="ps-4">Item No</th>
-                <th className="ps-4">Unit Price</th>
-                <th className="ps-4">Total Price</th>
-                <th className="ps-4">Date</th>
-                <th className="ps-4">Customer Name</th>
-                <th className="ps-4">Customer Phone No</th>
-                <th className="ps-4">Status</th>
-                <th className="ps-4"></th>
-              </tr>
-              <tr className="orderListItems text-white">
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">
-                  <FontAwesomeIcon size="2x" icon={faEdit} />{" "}
-                  <FontAwesomeIcon size="2x" icon={faTrash} />
-                </td>
-              </tr>
-              <tr className="orderListItems text-white">
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">
-                  <FontAwesomeIcon size="2x" icon={faEdit} />{" "}
-                  <FontAwesomeIcon size="2x" icon={faTrash} />
-                </td>
-              </tr>
-              <tr className="orderListItems text-white">
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">Test</td>
-                <td className="ps-4">
-                  <FontAwesomeIcon size="2x" icon={faEdit} />{" "}
-                  <FontAwesomeIcon size="2x" icon={faTrash} />
-                </td>
-              </tr>
+            <table class="table table-bordered  order">
+        
+                <tr class="orderListItems">
+                  <th className="ps-4">Test1</th>
+                  <th className="ps-4">Test 2</th>
+                  <th className="ps-4">Test 3</th>
+                  <th className="ps-4">Test 4</th>
+                  <th className="ps-4">Test 5</th>
+                  <th className="ps-4">Test 6</th>
+                  <th className="ps-4"></th>
+                </tr>
+  
+                {orders.map((order)  => {
+                  return (<tr key={order.orderId} class="orderListItems text-white">
+                    <td className="ps-4">{order.customerName}</td>
+                    <td className="ps-4">Test</td>
+                    <td className="ps-4">Test</td>
+                    <td className="ps-4">{order.customerName}</td>
+                    <td className="ps-4">Test</td>
+                    <td className="ps-4">Test</td>
+                    <td className="ps-4">
+                      <FontAwesomeIcon size="2x" icon={faEdit} />
+                      <FontAwesomeIcon size="2x" icon={faTrash} />
+                    </td>
+                  </tr>)
+                })}
+
             </table>
           </div>
         </div>
