fcff94f5ce37a4f43feee6fe099a2ec90a16a867
tlorence <tirone.lorence@gmail.com>
2021-09-26 00:23:00 +0530

    Login

---
+++ Releases/SourceCode/Backend/untitled/src/Document.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+
+public class Document {
+
+    private String[] authors;
+    private Date date;
+
+    public String[] getAuthors(){
+        return authors;
+    };
+
+    public void addAuthors(String name){
+
+    }
+
+    public Date getDate(){
+        return date;
+    }
+}
+
