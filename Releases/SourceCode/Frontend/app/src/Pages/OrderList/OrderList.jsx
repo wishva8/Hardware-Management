53a1841abd0d71f9b1d26fd5b7323095bb523819
Savvy9899 <pamuditha.rasanjana99@gmail.com>
2021-09-25 23:58:41 +0530

    detail order report generation done

--- Releases/SourceCode/Frontend/app/src/Pages/OrderList/OrderList.jsx
+++ Releases/SourceCode/Frontend/app/src/Pages/OrderList/OrderList.jsx
@@ -10,6 +10,7 @@ import { orderURL } from "../../Services/endpoints";
 import { Link, Redirect } from "react-router-dom";
 import Swal from "sweetalert2";
 import { deleteOrderURL } from "../../Services/endpoints";
+import generatePDFOrders from "./OrderReport";
 
 export default class OrderList extends Component {
   state = {
@@ -103,7 +104,12 @@ export default class OrderList extends Component {
             >
               <FontAwesomeIcon icon={faPlus} /> Add Order
             </button>
-            <button type="submit" className="Order-Button-Report">
+            <button
+              className="Order-Button-Report"
+              onClick={() => {
+                generatePDFOrders(this.state.orders);
+              }}
+            >
               <FontAwesomeIcon icon={faDownload} /> Report
             </button>
           </div>
